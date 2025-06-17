48d07585039af26587e347c564ccff04a4fb1abe
tawsif-r <urrahmantawsif@gmail.com>
2025-06-17 19:50:14 +0600

    dockerize

--- pharmacy_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-model_arr = [Pharmacy_owner,Pharmacy,Medicine,Customer,Order,OrderItem,ShippingAddress]
-for i in model_arr:
-    admin.site.register(i) 
--- store/admin.py
+++
@@ -1,7 +0,0 @@
-# Register your models here.
-from django.contrib import admin
-from .models import *
-
-model_arr = [Finaled_order]
-for i in model_arr:
-    admin.site.register(i) 
