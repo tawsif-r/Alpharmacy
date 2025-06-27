01c4743ad49cdfa1d2ddfd525bbe31cf30f03193
tawsif-r <urrahmantawsif@gmail.com>
2025-06-27 17:23:08 +0600

    made views

--- alpharmacy/product/views.py
+++ alpharmacy/product/views.py
@@ -1,5 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets,status
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 # Create your views here.
 def test(request):
     return HttpResponse("hello this is products")
+
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        products = Product.objects.all()
+        serializers = ProductSerializer(products, many=True)
+        return Response(serializers.data,status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, pk=None):
+        try:
+            product = Product.objects.get(id=pk)
+            serializer = ProductSerializer(product)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Product.DoesNotExist:
+            return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+    def create(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk=None):
+        try:
+            product = Product.objects.get(id=pk)
+            serializer = ProductSerializer(product, datat=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Product.DoesNotExist:
+            return Response({"error": "product not found"},status=status.HTTP_404_NOT_FOUND)
+        
+    def destroy(self, request, pk=None):
+        try:
+            product = Product.objects.get(id=pk)
+            product.delete()
+            return Response({"message": "Product deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+        except Product.DoesNotExist:
+            return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
